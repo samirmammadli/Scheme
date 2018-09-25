f8648cab9ff285779145cafe3a65a2e27cb282f3
Samir Mammadli <mammadli.s86@gmail.com>
2018-09-25 11:00:38 +0400

    1

---
+++ OutputDataConvert/BacklogOutput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scheme.OutputDataConvert
+{
+    public class Backlog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public short ColumnType { get; set; }
+    }
+}
---
+++ OutputDataConvert/SprintOutput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scheme.OutputDataConvert
+{
+    public class SprintOutput
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ExpireDate { get; set; }
+    }
+}
