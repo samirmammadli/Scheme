7b5f3bbd4a6f7e2dc1f5acce8a277a3c84d6224f
Samir Mammadli <mammadli.s86@gmail.com>
2018-09-25 17:07:28 +0400

    1

--- Tools/Extension Methods/ColumnContextExtensions.cs
+++ Tools/Extension Methods/ColumnContextExtensions.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Scheme.Entities;
+using Scheme.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Scheme.Tools.Extension_Methods
 {
-    public class ColumnContextExtensions
+    public static class ColumnContextExtensions
     {
+        private static ControllerErrorCode _code;
 
+        public static ControllerErrorCode GetError(this DbSet<Column> columns)
+        {
+            return _code;
+        }
+
+        public async static Task<IEnumerable<Column>> GetColumns(this ProjectContext db, string userEmail, int projectId)
+        {
+            var user = await db.Users.FirstOrDefaultAsync(x => x.Email.Equals(userEmail, StringComparison.OrdinalIgnoreCase));
+
+            if (user == null)
+            {
+                _code = ControllerErrorCode.UserNotFound;
+                return null;
+            }
+
+            var project = await db.Projects.FirstOrDefaultAsync(x => x.Id == projectId);
+
+            if (project == null)
+            {
+                _code = ControllerErrorCode.ProjectNotFound;
+                return null;
+            }
+
+            var role = await db.Roles.AsNoTracking().FirstOrDefaultAsync(x => x.User == user && x.Project == project);
+
+            if (role == null)
+            {
+                _code = ControllerErrorCode.PermissionsDenied;
+                return null;
+            }
+
+            var columns = await db.Columns.AsNoTracking().Where(x => x.Project == project).ToListAsync();
+
+            return columns;
+
+        }
     }
 }
